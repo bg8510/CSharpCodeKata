c5ef317cc6a61c3c55dce42f4d5f5c32b9247804
Brooks Gatlin <gbrooks8510@excite.com>
2019-07-23 12:33:38 -0500

    Changed a tangled set of 'if' statements to a cleaner 'switch' statement

--- src/ProviderQuality.Console/Program.cs
+++ src/ProviderQuality.Console/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;                                                  // Brooks
 
 namespace ProviderQuality.Console
 {
@@ -25,154 +24,109 @@ namespace ProviderQuality.Console
                     new Award {Name = "Blue Distinction Plus", ExpiresIn = 0, Quality = 80},
                     new Award {Name = "Blue Compare", ExpiresIn = 15, Quality = 20},
                     new Award {Name = "Top Connected Providers", ExpiresIn = 3, Quality = 6},
-                    new Award {Name = "Blue Star", ExpiresIn = 30, Quality = 50}
+                    new Award {Name = "Blue Star", ExpiresIn = 10, Quality = 50}
                 }
 
             };
 
-            while (true)                                                         // Brooks debug
-            {
                 app.UpdateQuality();
 
                 System.Console.ReadKey();
-            }
-
         }
 
         public void UpdateQuality()
         {
             for (var i = 0; i < Awards.Count; i++)
             {
-                if (Awards[i].Name != "Blue First" && Awards[i].Name != "Blue Compare")
+                switch (Awards[i].Name)
                 {
-                    if (Awards[i].Quality > 0)
-                    {
-                        if (Awards[i].Name != "Blue Distinction Plus")
-                        {
-                            Awards[i].Quality = Awards[i].Quality - 1;
-                        }
+                    case "Blue Distinction Plus":
+                        // This case is necessary to prevent "Blue Distinction Plus" from going to the default case
+                        break;
 
-                        // Blue Star gets an extra point docked
-                        if (Awards[i].Name == "Blue Star")
-                        {
-                            Awards[i].Quality = Awards[i].Quality - 1;
-                        }
-                    }
-                }
-                // This 'else' applies to Blue First and Blue Compare, the two increasers
-                else
-                {
-                    if (Awards[i].Quality < 50)
-                    {
-                        Awards[i].Quality = Awards[i].Quality + 1;
-
-                        // Blue Compare gets 2 more tiers, at 10 days and at 5 days
-                        if (Awards[i].Name == "Blue Compare")
-                        {
-                            if (Awards[i].ExpiresIn < 11)
-                            {
-                                if (Awards[i].Quality < 50)
-                                {
-                                    Awards[i].Quality = Awards[i].Quality + 1;
-                                }
-                            }
-
-                            if (Awards[i].ExpiresIn < 6)
-                            {
-                                if (Awards[i].Quality < 50)
-                                {
-                                    Awards[i].Quality = Awards[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                // Blue Distinction Plus doesn't decrease in Quality, just counts down the days
-                if (Awards[i].Name != "Blue Distinction Plus")
-                {
-                    Awards[i].ExpiresIn = Awards[i].ExpiresIn - 1;
-                }
-
-                // When ExpiresIn goes negative, apply the extra decrease
-                if (Awards[i].ExpiresIn < 0)
-                {
-                    if (Awards[i].Name != "Blue First")
-                    {
-                        if (Awards[i].Name != "Blue Compare")
-                        {
-                            if (Awards[i].Quality > 0)
-                            {
-                                if (Awards[i].Name != "Blue Distinction Plus")
-                                {
-                                    Awards[i].Quality = Awards[i].Quality - 1;
-                                }
-
-                                // Blue Star gets an extra point docked
-                                if (Awards[i].Name == "Blue Star")
-                                {
-                                    Awards[i].Quality = Awards[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Awards[i].Quality = Awards[i].Quality - Awards[i].Quality;
-                        }
-                    }
-                    else
-                    {
+                    case "Blue First":
                         if (Awards[i].Quality < 50)
                         {
-                            Awards[i].Quality = Awards[i].Quality + 1;
+                            Awards[i].Quality++;
                         }
-                    }
+
+                        if ((Awards[i].ExpiresIn <= 0) && Awards[i].Quality < 50)
+                        {
+                            Awards[i].Quality++;
+                        }
+
+                        break;
+
+                    case "Blue Compare":
+                        if (Awards[i].ExpiresIn <= 0)
+                        {
+                            Awards[i].Quality = 0;
+                        }
+                        else if (Awards[i].Quality < 50)
+                        {
+                            Awards[i].Quality++;
+
+                            // Blue Compare gets 2 more tiers, at 10 days and at 5 days
+                            if (Awards[i].ExpiresIn < 11 && Awards[i].Quality < 50)
+                            {
+                                Awards[i].Quality++;
+                            }
+
+                            if (Awards[i].ExpiresIn < 6 && Awards[i].Quality < 50)
+                            {
+                                Awards[i].Quality++;
+                            } 
+                        }
+
+                        break;
+
+                    case "Blue Star":
+                        if (Awards[i].Quality == 1)
+                        {
+                            Awards[i].Quality = 0;
+                        }
+
+                        if (Awards[i].Quality > 1)
+                        {
+                            Awards[i].Quality -= 2;
+                        }
+
+                        if ((Awards[i].ExpiresIn <= 0))
+                        {
+                            // Subtract 2 if possible, otherwise it stops at zero
+                            if (Awards[i].Quality > 1)
+                            {
+                                Awards[i].Quality -= 2;
+                            }
+                            else
+                            {
+                                Awards[i].Quality = 0;
+                            }
+                        }
+
+                        break;
+
+                    // These cases aren't necessary, they're here for clarity
+                    case "Gov Quality Plus":
+                    case "ACME Partner Facility":
+                    case "Top Connected Providers":
+                    default:
+                        if (Awards[i].Quality > 0)
+                        {
+                            Awards[i].Quality--;
+                        }
+
+                        if (Awards[i].Quality > 0 && Awards[i].ExpiresIn <= 0)
+                        {
+                            Awards[i].Quality--;
+                        }
+                        
+                        break;
                 }
 
-                //switch (Awards[i].Name)
-                //    {
-                //        case "Blue First":
-                //            if (Awards[i].Quality < 50)
-                //            {
-                //                Awards[i].Quality++;
-                //            }
-                //            break;
-
-                //        case "Blue Distinction Plus":
-                //            break;
-
-                //        case "Blue Compare":
-                //            break;
-
-                //        case "Blue Star":
-                //            break;
-
-                //        case "Gov Quality Plus":
-                //        case "ACME Partner Facility":
-                //        case "Top Connected Providers":
-
-                //            break;
-
-                //        default:
-                //            break;
-                //    }
-
+                Awards[i].ExpiresIn--;
 
             }
-
-
-            ///////////////////// Brooks - for debug only
-            IEnumerable<Award> sortedEnum = Awards.OrderBy(award => award.Name);
-            IList<Award> sortedAwardList = sortedEnum.ToList();
-
-            foreach (Award a in sortedAwardList)
-            {
-                System.Console.WriteLine("\n" + a.Name + "\n Exp  " + a.ExpiresIn + "\t Q " + a.Quality);
-            }
-
-            /////////////////////
         }
-
     }
-
 }
